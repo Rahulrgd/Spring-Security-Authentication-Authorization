00074db121d1ed69c855b777f5f90e0682a7b5ae
RahulGupta <rahulrgd21@gmail.com>
2024-06-23 14:55:21 +0530

    Created UserRepository Class

---
+++ src/main/java/com/project/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
