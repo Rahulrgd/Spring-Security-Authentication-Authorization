f6a074f55a3e2ccdc6da37a33863f7253458fbf3
RahulGupta <rahulrgd21@gmail.com>
2024-06-23 16:03:18 +0530

    Created extractUsername method in jwtServices

--- src/main/java/com/project/service/jwtService.java
+++ src/main/java/com/project/service/jwtService.java
@@ -16,6 +16,10 @@ public class jwtService {
   private final String SECRET_KEY =
     "8dd2b0543e7d9f7334f15a41ec4c22ad5c330d327fd64733dbf090298ecfa551";
 
+    public String extractUsername(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
     public <T> T extractClaims(String token, Function<Claims, T> resolver){
         Claims claims = extractAllClaims(token);
         return resolver.apply(claims);
