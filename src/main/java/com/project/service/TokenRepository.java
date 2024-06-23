aecb2f2cfb15bcbbf59bcc94dd64567b14c0bc6b
RahulGupta <rahulrgd21@gmail.com>
2024-06-23 20:38:16 +0530

    Implemented AuthenticationService & JwtService Classes

--- src/main/java/com/project/service/TokenRepository.java
+++ src/main/java/com/project/service/TokenRepository.java
@@ -2,4 +2,9 @@ package com.project.service;
 
 public class TokenRepository {
 
+    public Object findByAccessToken(String token) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByAccessToken'");
+    }
+
 }
