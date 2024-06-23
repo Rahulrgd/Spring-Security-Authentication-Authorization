51cd1c6dd7f8b19efbdcf320f042af562fcf9279
RahulGupta <rahulrgd21@gmail.com>
2024-06-23 20:52:58 +0530

    Created CustomLogOutHandler Class, Externalised few configurations of application to yml file, Implemented PasswordEncoder mean in Security Config

--- src/main/java/com/project/controller/AuthenticationController.java
+++ src/main/java/com/project/controller/AuthenticationController.java
@@ -1,12 +1,45 @@
 package com.project.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.model.AuthenticationResponse;
+import com.project.model.User;
 import com.project.service.AuthenticationService;
 
 @RestController
 public class AuthenticationController {
-    
+
     private final AuthenticationService authService;
-    
+
+    public AuthenticationController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody User request
+            ) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody User request
+    ) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/refresh_token")
+    public ResponseEntity refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        return authService.refreshToken(request, response);
+    }
 }
