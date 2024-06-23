7cd8698cc07ddd826f83a04b564cb6ceebf914c1
RahulGupta <rahulrgd21@gmail.com>
2024-06-23 15:29:15 +0530

    Create generateToken method in jwtService.

--- src/main/java/com/project/service/jwtService.java
+++ src/main/java/com/project/service/jwtService.java
@@ -1,7 +1,29 @@
 package com.project.service;
 
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import java.util.Base64.Decoder;
+
+import com.project.model.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 public class jwtService {
 
   private final String SECRET_KEY =
     "8dd2b0543e7d9f7334f15a41ec4c22ad5c330d327fd64733dbf090298ecfa551";
+
+    public String generateToken(User user){
+        String token = Jwts.builder().subject(user.getUsername()).issuedAt(new Date(System.currentTimeMillis())).expiration(new Date(System.currentTimeMillis() + 24*60*60*1000)).signWith(getSigninKey()).compact();
+        return token;
+    }
+
+    private SecretKey getSigninKey(){
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
